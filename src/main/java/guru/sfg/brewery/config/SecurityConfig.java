60ad72ce7a066f282435ee068203bf353058a143
adultmalehuman <adultmalehuman@gmail.com>
2020-11-22 16:52:38 -0400

    Creating a custom authentication filter (part 3) - disabled CSRF for API requests

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -31,8 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
                 http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
-                //.csrf().disable()
-                ;
+                .csrf().disable();
 
                 http
                 .authorizeRequests(authorize -> {
